2ad3b388bf2d17f1856b1cdf0d4f227f720b77a6
rex@billion <anderson.aristotle@gmail.com>
2023-05-27 14:36:15 -0400

    initial commit w/data

---
+++ README.md
@@ -0,0 +1,1 @@
+# ncrease-mongodb-data
---
+++ data.js
@@ -0,0 +1,37 @@
+const data = [
+  {
+    term: "Credit Score",
+    definition:
+      "A numerical representation of an individual's creditworthiness based on their credit history.",
+    example: "John's credit score is 750, indicating a good credit standing.",
+    source:
+      "Source: Experian - https://www.experian.com/credit/what-is-a-good-credit-score.html",
+  },
+  {
+    term: "Credit Report",
+    definition:
+      "A detailed record of an individual's credit history, including their borrowing and repayment activities.",
+    example:
+      "Sarah reviewed her credit report and found no errors or discrepancies.",
+    source: "Source: TransUnion - https://www.transunion.com/credit-reports",
+  },
+  {
+    term: "FICO Score",
+    definition:
+      "A credit scoring model developed by the Fair Isaac Corporation (FICO), widely used to assess creditworthiness.",
+    example: "Mark's FICO score is 680, which falls in the fair credit range.",
+    source:
+      "Source: myFICO - https://www.myfico.com/credit-education/credit-scores",
+  },
+  {
+    term: "Credit Utilization Ratio",
+    definition:
+      "The percentage of a person's available credit that they are currently using, often considered in credit scoring models.",
+    example:
+      "Jane's credit utilization ratio is 30%, indicating responsible credit management.",
+    source:
+      "Source: Equifax - https://www.equifax.com/personal/education/credit/score/what-is-a-good-credit-utilization-ratio/",
+  },
+];
+
+console.log(data);
